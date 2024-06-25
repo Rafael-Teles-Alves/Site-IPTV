63a2237a9717fda55c63510101cc278e87e7ba37
Rafael Teles <94096413+Rafael-Teles-Alves@users.noreply.github.com>
2024-06-24 21:37:58 -0300

    Update script_planos.js
    melhorando a escritas dos planos

--- script_planos.js
+++ script_planos.js
@@ -1,18 +1,10 @@
 
-
 // funçoes de enviar mesagem conforme plano
 
 function enviarWhat1() {
     var numeroTelefone = '+5511941365448'; 
-    var mensagem = "Olá, gostaria de assinar o plano de R$30/mês que inclui:\n\n" +
-        "+100.000 Conteúdos\n" +
-        "1 Tela\n" +
-        "Qualidade HD/SD/FHD e 4K + HDR\n" +
-        "Filmes, Séries, Canais e Rádios\n" +
-        "Assista no Smartphone/Tablet\n" +
-        "Assista na Smart TV\n" +
-        "Assista no TV Box/Chromecast\n" +
-        "Programação Adultos [Opcional]";
+    var mensagem = "Olá, gostaria de assinar o plano de R$30/mês :";
+  
     var url = "https://api.whatsapp.com/send?phone=" + numeroTelefone + "&text=" + encodeURIComponent(mensagem);
     window.open(url, '_blank');
 }
@@ -21,15 +13,7 @@ function enviarWhat1() {
 
 function enviarWhat2() {
     var numeroTelefone = '+5511941365448'; 
-    var mensagem = "Olá, gostaria de assinar o plano de R$80/mês que inclui:\n\n" +
-        "+100.000 Conteúdos\n" +
-        "1 Tela\n" +
-        "Qualidade HD/SD/FHD e 4K + HDR\n" +
-        "Filmes, Séries, Canais e Rádios\n" +
-        "Assista no Smartphone/Tablet\n" +
-        "Assista na Smart TV\n" +
-        "Assista no TV Box/Chromecast\n" +
-        "Programação Adultos [Opcional]";
+    var mensagem = "Olá, gostaria de assinar o plano de R$80/mês:";
     var url = "https://api.whatsapp.com/send?phone=" + numeroTelefone + "&text=" + encodeURIComponent(mensagem);
     window.open(url, '_blank');
 }
@@ -38,15 +22,7 @@ function enviarWhat2() {
 
 function enviarWhat3() {
     var numeroTelefone = '+5511941365448'; 
-    var mensagem = "Olá, gostaria de assinar o plano de R$330/mês que inclui:\n\n" +
-        "+100.000 Conteúdos\n" +
-        "1 Tela\n" +
-        "Qualidade HD/SD/FHD e 4K + HDR\n" +
-        "Filmes, Séries, Canais e Rádios\n" +
-        "Assista no Smartphone/Tablet\n" +
-        "Assista na Smart TV\n" +
-        "Assista no TV Box/Chromecast\n" +
-        "Programação Adultos [Opcional]";
+    var mensagem = "Olá, gostaria de assinar o plano de R$330/mês:";
     var url = "https://api.whatsapp.com/send?phone=" + numeroTelefone + "&text=" + encodeURIComponent(mensagem);
     window.open(url, '_blank');
 }
