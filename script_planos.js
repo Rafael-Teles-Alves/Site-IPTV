a35778eee841517ce914b515886c7a582349361f
Rafael Teles <94096413+Rafael-Teles-Alves@users.noreply.github.com>
2024-06-25 00:28:22 -0300

    Update script_planos.js

--- script_planos.js
+++ script_planos.js
@@ -3,7 +3,7 @@
 
 function enviarWhat1() {
     var numeroTelefone = '+5511941365448'; 
-    var mensagem = "Olá, gostaria de assinar o plano de R$30/mês :";
+    var mensagem = "Olá, gostaria de assinar o PLANO MENSAL R$30/ Por Mês :";
   
     var url = "https://api.whatsapp.com/send?phone=" + numeroTelefone + "&text=" + encodeURIComponent(mensagem);
     window.open(url, '_blank');
@@ -13,7 +13,7 @@ function enviarWhat1() {
 
 function enviarWhat2() {
     var numeroTelefone = '+5511941365448'; 
-    var mensagem = "Olá, gostaria de assinar o plano de R$80/mês:";
+    var mensagem = "Olá, gostaria de assinar o PLANO TRIMESTRAL R$80/Por 3 Mês:";
     var url = "https://api.whatsapp.com/send?phone=" + numeroTelefone + "&text=" + encodeURIComponent(mensagem);
     window.open(url, '_blank');
 }
@@ -22,7 +22,7 @@ function enviarWhat2() {
 
 function enviarWhat3() {
     var numeroTelefone = '+5511941365448'; 
-    var mensagem = "Olá, gostaria de assinar o plano de R$330/mês:";
+    var mensagem = "Olá, gostaria de assinar o PLANO ANUAL R$330/ Por 12 mês:";
     var url = "https://api.whatsapp.com/send?phone=" + numeroTelefone + "&text=" + encodeURIComponent(mensagem);
     window.open(url, '_blank');
 }
