535df00ef73c80242e875757850e3dbe21b3e5c1
Rafael Teles <94096413+Rafael-Teles-Alves@users.noreply.github.com>
2024-06-24 21:03:15 -0300

    Update script_planos.js

--- script_planos.js
+++ script_planos.js
@@ -1,1 +1,52 @@
 
+
+// funçoes de enviar mesagem conforme plano
+
+function enviarWhat1() {
+    var numeroTelefone = '+5511941365448'; 
+    var mensagem = "Olá, gostaria de assinar o plano de R$30/mês que inclui:\n\n" +
+        "+100.000 Conteúdos\n" +
+        "1 Tela\n" +
+        "Qualidade HD/SD/FHD e 4K + HDR\n" +
+        "Filmes, Séries, Canais e Rádios\n" +
+        "Assista no Smartphone/Tablet\n" +
+        "Assista na Smart TV\n" +
+        "Assista no TV Box/Chromecast\n" +
+        "Programação Adultos [Opcional]";
+    var url = "https://api.whatsapp.com/send?phone=" + numeroTelefone + "&text=" + encodeURIComponent(mensagem);
+    window.open(url, '_blank');
+}
+
+
+
+function enviarWhat2() {
+    var numeroTelefone = '+5511941365448'; 
+    var mensagem = "Olá, gostaria de assinar o plano de R$80/mês que inclui:\n\n" +
+        "+100.000 Conteúdos\n" +
+        "1 Tela\n" +
+        "Qualidade HD/SD/FHD e 4K + HDR\n" +
+        "Filmes, Séries, Canais e Rádios\n" +
+        "Assista no Smartphone/Tablet\n" +
+        "Assista na Smart TV\n" +
+        "Assista no TV Box/Chromecast\n" +
+        "Programação Adultos [Opcional]";
+    var url = "https://api.whatsapp.com/send?phone=" + numeroTelefone + "&text=" + encodeURIComponent(mensagem);
+    window.open(url, '_blank');
+}
+
+
+
+function enviarWhat3() {
+    var numeroTelefone = '+5511941365448'; 
+    var mensagem = "Olá, gostaria de assinar o plano de R$330/mês que inclui:\n\n" +
+        "+100.000 Conteúdos\n" +
+        "1 Tela\n" +
+        "Qualidade HD/SD/FHD e 4K + HDR\n" +
+        "Filmes, Séries, Canais e Rádios\n" +
+        "Assista no Smartphone/Tablet\n" +
+        "Assista na Smart TV\n" +
+        "Assista no TV Box/Chromecast\n" +
+        "Programação Adultos [Opcional]";
+    var url = "https://api.whatsapp.com/send?phone=" + numeroTelefone + "&text=" + encodeURIComponent(mensagem);
+    window.open(url, '_blank');
+}
